2dca01dc4a147181431d6a6189a003c3f07bd8f5
Rainbow <tehrainbowguy@gmail.com>
2012-09-03 00:06:16 +0100

    removed statusthread

--- DistributedSHA.git/src/me/supermaxman/distributedsha/main/Main.java
+++ DistributedSHA.git/src/me/supermaxman/distributedsha/main/Main.java
@@ -3,7 +3,6 @@ package me.supermaxman.distributedsha.main;
 public class Main {
     static SocketThread socketThread;
     static HashingThread hashingThread;
-    static StatusThread statusThread;
     static int hashed = 0;
 
 
@@ -12,10 +11,9 @@ public class Main {
 
         socketThread = new SocketThread();
         hashingThread = new HashingThread();
-        statusThread = new StatusThread();
         socketThread.start();
         hashingThread.start();
-        statusThread.start();
+
 		new mainWindow();
 	}
 
